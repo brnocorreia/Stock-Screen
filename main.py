4ee90c2d2a84851d253455420fee86b8b598db07
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-07-08 01:49:34 -0300

    main functions nammed and home.html skeleton done

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def dashboard():
+def home(request: Request):
     """
     Displays the stock screener dashboard / homepage
     """
-    return {"Dashboard": "Home Page"}
+    return templates.TemplateResponse("home.html", {
+        "request": request,
+    })
 
 @app.post("/stock")
 def create_stock():
